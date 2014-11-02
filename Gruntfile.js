ba958736b327834119a6de53e9e4c14e70c6c62f
Alyssa <alyssaquek@gmail.com>
2014-11-03 03:40:08 +0800

    Filter multiVs with at least 12 vitamins and sort by price per serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,26 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   var nutrients =  grunt.file.readJSON('app/data/nutrients.json');
+  var data = grunt.file.readJSON('app/data/results.json');
+  var filtered = [];
+  data.reduce(function (arr, row) {
+    if (row.nutrients.Vitamins["Vitamin K"] && row.nutrients.Vitamins["Vitamin K"][2] >= 100
+      && row.num_Vitamins >= 12) {
+      arr.push(row);
+    }
+    return arr;
+  }, filtered);
+
+  filtered.sort(function (rowa, rowb) {
+    return rowa.price_per_serve - rowb.price_per_serve;
+  });
+
   grunt.initConfig({
     nunjucks: {
       options: {
         paths: ['app/templates'],
         data: {
-          data: grunt.file.readJSON('app/data/results.json'),
+          data: filtered,
           categories: ['Vitamins', 'Minerals', 'Macronutrients',
             'Trace Elements', 'Amino Acids', 'Enzymes'],
           allnutrients: nutrients
