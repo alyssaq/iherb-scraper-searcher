b9ffecc9953a672632a7e42ced01cbf89c3e0677
Alyssa <alyssaquek@gmail.com>
2014-11-04 20:01:11 +0800

    Swapped to grunt-nunjucks to precompile templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,56 @@
 'use strict';
 
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to match all subfolders:
+// 'test/spec/**/*.js'
+// templateFramework: 'lodash'
+
 module.exports = function (grunt) {
   require('time-grunt')(grunt);
   require('load-grunt-tasks')(grunt);
 
-  var nutrients =  grunt.file.readJSON('app/data/nutrients.json');
-  var data = grunt.file.readJSON('app/data/results.json');
-  var filtered = [];
-  data.reduce(function (arr, row) {
-    if (row.nutrients.Vitamins["Vitamin K"] && row.nutrients.Vitamins["Vitamin K"][2] >= 100
-      && row.num_Vitamins >= 12) {
-      arr.push(row);
-    }
-    return arr;
-  }, filtered);
-
-  filtered.sort(function (rowa, rowb) {
-    return rowa.price_per_serve - rowb.price_per_serve;
-  });
+  var config = {
+    app: 'app',
+    dest: 'dest'
+  };
 
   grunt.initConfig({
+    config: config,
+
     nunjucks: {
-      options: {
-        paths: ['app/templates'],
-        data: {
-          data: filtered,
-          categories: ['Vitamins', 'Minerals', 'Macronutrients',
-            'Trace Elements', 'Amino Acids', 'Enzymes'],
-          allnutrients: nutrients
+      precompile: {
+        baseDir: '<%= config.app %>/templates/',
+        src: '<%= config.app %>/templates/results.html',
+        dest: '<%= config.dest %>/js/templates.js',
+        options: {
+          //env: require('./nunjucks-environment')
         }
-      },
-      render: {
-        files: [
-           {
-              expand: true,
-              cwd: "app/templates/",
-              src: "index.html",
-              dest: "app/",
-              ext: ".html"
-           }
-        ]
+      }
+    },
+
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.dest %>',
+          src: [
+            'index.html',
+            '*.{ico,txt}',
+            'data/*.json',
+            'css/{,*/}*.css',
+            'js/{,*/}*.js'
+          ]
+        }]
       }
     }
+
   });
 
   grunt.registerTask('default', [
-    'nunjucks'
+    'nunjucks', 'copy'
   ]);
 };
