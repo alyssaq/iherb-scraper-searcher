61ca52c1a9c4be50d3d69a1f9f30edff3f751333
Alyssa <alyssaquek@gmail.com>
2014-11-04 21:07:28 +0800

    Added grunt build tasks - cssmin, copy, livereload, connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,10 @@
 'use strict';
+var LIVERELOAD_PORT = 35729;
+var SERVER_PORT = 9000;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
 
 // # Globbing
 // for performance reasons we're only matching one level down:
@@ -23,9 +29,17 @@ module.exports = function (grunt) {
       precompile: {
         baseDir: '<%= config.app %>/templates/',
         src: '<%= config.app %>/templates/results.html',
-        dest: '<%= config.dest %>/js/templates.js',
-        options: {
-          //env: require('./nunjucks-environment')
+        dest: '<%= config.dest %>/js/templates.js'
+      }
+    },
+
+    cssmin: {
+      dest: {
+        files: {
+          '<%= config.dest %>/css/site.css': [
+            '.tmp/css/*.css',
+            '<%= config.app %>/css/*.css'
+          ]
         }
       }
     },
@@ -41,16 +55,117 @@ module.exports = function (grunt) {
             'index.html',
             '*.{ico,txt}',
             'data/*.json',
-            'css/{,*/}*.css',
             'js/{,*/}*.js'
           ]
         }]
       }
-    }
+    },
 
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: true
+      },
+      nunjucks: {
+        files: ['<%= config.app %>/templates/*.html'],
+        tasks: ['nunjucks']
+      },
+      css: {
+        files: ['<%= config.app %>/css/*.css'],
+        tasks: ['cssmin']
+      },
+      copy: {
+        files: ['index.html', '<%= config.app %>/js/{,*/}*.js', '<%= config.app %>/data/*.json'],
+        tasks: ['copy']
+      },
+      livereload: {
+        options: {
+          livereload: grunt.option('livereloadport') || LIVERELOAD_PORT
+        },
+        files: [
+          '<%= config.app %>/*.html',
+          '{.tmp,<%= config.app %>}/css/{,*/}*.css',
+          '{.tmp,<%= config.app %>}/js/{,*/}*.js',
+          '<%= config.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+          '<%= config.app %>/templates/*.{html,ejs,mustache,hbs}'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: grunt.option('port') || SERVER_PORT,
+        hostname: '0.0.0.0'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, config.dest)
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'test'),
+              mountFolder(connect, config.app)
+            ];
+          }
+        }
+      },
+      dest: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, config.dest)
+            ];
+          }
+        }
+      }
+    },
+
+    open: {
+      server: {
+        path: 'http://0.0.0.0:<%= connect.options.port %>'
+      },
+      test: {
+        path: 'http://0.0.0.0:<%= connect.test.options.port %>'
+      }
+    },
+
+    clean: {
+      dest: ['.tmp', '<%= config.dest %>/*']
+    }
   });
 
+  grunt.registerTask('dev', [
+    'build',
+    'connect:livereload',
+    'open:server',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'nunjucks',
+
+    // 'useminPrepare',
+    // 'imagemin',
+    // 'htmlmin',
+    // 'concat',
+    'cssmin',
+    'copy'
+  ]);
+
   grunt.registerTask('default', [
-    'nunjucks', 'copy'
+    'build'
   ]);
 };
