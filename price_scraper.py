c3598a93a6355794c2f26f06dc090b56b284e492
Alyssa <alyssaquek@gmail.com>
2015-04-24 10:29:51 +0800

    size regex

--- price_scraper.py
+++ price_scraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+from __future__ import division
 import sys
 import requests
 import re
@@ -8,8 +8,10 @@ import json
 import gevent
 from bs4 import BeautifulSoup, NavigableString
 import itertools
+import functools
 flatten = itertools.chain.from_iterable
 
+GRAM_REGEX = re.compile(r'(\d{1,2}\.?\d{0,2})\s?g')
 DOMAIN = 'http://www.iherb.com'
 UNICODE_REGEX = re.compile(r'[^\x00-\x7f]|\r')
 SERVING_TEXT_REGEX = re.compile(
@@ -18,7 +20,7 @@ SERVING_TEXT_REGEX = re.compile(
 PRICE_REGEX = re.compile(r'\s*\$(\d{1,2}\.?\d{0,2})\s*')
 NON_DIGITS_REGEX = re.compile(r'<|,|\*|%')
 SIZE_REGEX = re.compile(r'(\.?\d{1,4}\.?\/?\d{0,3})\s?([a-zA-Z \-]+)')
-SIZE_PARTITION_REGEX = re.compile(r',|or |\(|\)')
+SIZE_PARTITION_REGEX = re.compile(r';|,|or |\(|\)')
 NUMBER_MAP = {
   'each': 1,
   'one': 1,
@@ -30,6 +32,28 @@ NUMBER_MAP = {
   'seven': 7,
   'eight': 8
 }
+UNIT_CONVERSIONS = [{
+  'unit': 'lbs',
+  'multiplier': 0.00220462
+}, {
+  'unit': 'oz',
+  'multiplier': 0.035274
+}, {
+  'unit': 'kg',
+  'multiplier': 0.001
+}, {
+  'unit': 'mg',
+  'multiplier': 1000
+}, {
+  'unit': 'floz',
+  'multiplier': 0.033814
+}, {
+  'unit': 'ml',
+  'multiplier': 1
+}, {
+  'unit': 'l',
+  'multiplier': 0.001
+}]
 
 def clean(unistr):
   decoded = re.sub(UNICODE_REGEX, '', unistr)
@@ -56,25 +80,25 @@ def get_serving_text(facts_table):
   return clean(text)
 
 def get_container_size_text(text):
-  return ','.join(text.split(',')[-2:])
+  return ';'.join(map(lambda x: x.replace(',', ''), text.split(', ')[-2:]))
 
 def to_number(num):
   divider = '/'
   decimal = '.'
-  try:
-    if divider in num:
-      fraction = num.split(divider)
-      return float(fraction[0]) / float(fraction[1])
-    elif decimal in num:
-      return float(num)
-    else:
+  if divider in num:
+    fraction = num.split(divider)
+    return float(fraction[0]) / float(fraction[1])
+  elif decimal in num:
+    return float(num)
+  else:
+    try:
       return int(num)
-  except:
-    return None
+    except:
+      return num
 
 def get_alphanumber_size(text):
   tokens = re.split(r'\s+', text)
-  amount = None
+  amount = 1
   unit_tokens = []
 
   # unit word too long, take the first word
@@ -86,24 +110,23 @@ def get_alphanumber_size(text):
       end_index = 1 if len(unit_tokens) > 5 else len(unit_tokens)
       unit_tokens = unit_tokens[:end_index]
 
-  return None if amount is None else {
+  return {
     'amount': amount,
     'unit': ' '.join(unit_tokens)
   }
 
 def get_sizes(text):
   partitions = re.split(SIZE_PARTITION_REGEX, text)
-
   sizes = []
   for part in reversed(partitions):
     match = SIZE_REGEX.search(part)
     if match:
+      print match.groups()
       amount, unit = match.groups()
-      if amount:
-        sizes.append({
-          'amount': to_number(amount),
-          'unit': unit.strip()
-        })
+      sizes.append({
+        'amount': to_number(amount),
+        'unit': unit.strip().lower()
+      })
 
   if len(sizes) == 0:
     sizes.append(get_alphanumber_size(text))
@@ -130,6 +153,10 @@ def percent_to_num(field):
 def price_to_float(field):
   match = PRICE_REGEX.match(field)
   return field if match is None else to_number(match.group(1))
+
+def gram_amount(field):
+  match = GRAM_REGEX.match(field)
+  return to_number(field) if match is None else to_number(match.group(1))
 
 def get_fact_table_rows(facts_table):
   rows = []
@@ -200,12 +227,30 @@ def get_display_name(text):
   text = text.replace('/', '/ ')
   return ','.join(text.split(',')[:-1])
 
+def unit_amount(fullsize, unit):
+  for size in fullsize:
+    if size['unit'] == unit or size['unit'].startswith(unit):
+      return size['amount']
+  return 1
+
 def init_profile():
   profile = {'nutrients': {}, 'num_nutrients': 0}
   for category, nutrientlist in ALL_NUTRIENTS.iteritems():
     profile['num_' + category] = 0
 
   return profile
+
+def price_per_g(price, sizes):
+  unit_amount_func = functools.partial(unit_amount, sizes)
+  price_per_g = price / unit_amount_func('g')
+
+  if price_per_g == price:
+    for converter in UNIT_CONVERSIONS:
+      unit_price = price / unit_amount_func(converter['unit'])
+      if unit_price != price:
+        return unit_price * converter['multiplier']
+
+  return price_per_g
 
 def product_profile(html):
   soup = BeautifulSoup(html)
@@ -218,45 +263,45 @@ def product_profile(html):
     return None
 
   profile = init_profile()
-  serving_text = None
+  serving_text = ''
   for table in sorted(tables, key=len, reverse=True):
     fill_nutrients_profile(table, profile)
     text = get_serving_text(table)
-    serving_text = text if serving_text is None and text != '' else serving_text
+    serving_text = text if serving_text == '' and text != '' else serving_text
 
-  if serving_text is None: return None
   profile['name'] = main.find('h1').text
   profile['display_name'] = get_display_name(profile['name'])
   price = profile['price'] = price_to_float(price.text)
 
-  profile['serving_text'] = serving_text
-  serves = profile['serving_sizes'] = get_sizes(profile['serving_text'])
+  # profile['serving_text'] = serving_text
+  # serves = profile['serving_sizes'] = get_sizes(profile['serving_text'])
   profile['container_text'] = get_container_size_text(profile['name'])
   fullsizes = profile['container_sizes'] = get_sizes(profile['container_text'])
+  profile['price_per_g'] = price_per_g(price, fullsizes)
 
-  full_index, serve_index = overlapping_sizes(fullsizes, serves)
-  full_amt = fullsizes[full_index]['amount']
-  serve_amt = serves[serve_index]['amount'] if serve_index >= 0 else full_amt
-  profile['size_indexes'] = [full_index, serve_index]
-  profile['price_per_unit'] = profile['price'] / (full_amt*1.0)
-  profile['price_per_serve'] = profile['price_per_unit'] * serve_amt
+  # protein_serve = unit_amount(serves, 'g')
+  # protein_amount = gram_amount(profile['nutrients']['Protein']['amount'])
+  # profile['protein_percent'] = (protein_amount / protein_serve) * 100
+
+  # full_index, serve_index = overlapping_sizes(fullsizes, serves)
+  # full_amt = fullsizes[full_index]['amount']
+  # serve_amt = serves[serve_index]['amount'] if serve_index >= 0 else full_amt
+  # profile['size_indexes'] = [full_index, serve_index]
+  # profile['price_per_unit'] = profile['price'] / (full_amt*1.0)
+  # profile['price_per_serve'] = profile['price_per_unit'] * serve_amt
 
   return profile
-
-i = 1
 
 def passed_jobs(jobs):
   for job in jobs:
     if job.value.status_code == 200:
       yield job.value
 
-def process(jobs):
+def process(jobs, index):
   profiles = []
-  global i
-
   for val in passed_jobs(jobs):
-    print('{0}) Processing: {1}'.format(i, val.url))
-    i = i + 1
+    index = index + 1
+    print('{0}) Processing: {1}'.format(index, val.url))
     profile = product_profile(val.text)
     if profile is not None:
       profile.update({'url': val.url + '?rcode=KQM091'})
@@ -279,7 +324,7 @@ def next_result_page(category):
     else:
       break
 
-def process_category(filename, category='multivitamins'):
+def process_category(filename, category='whey-protein'):
   results = []
 
   for page in next_result_page(category):
@@ -289,10 +334,9 @@ def process_category(filename, category='multivitamins'):
             for link in links]
     if len(jobs) > 0:
       gevent.wait(jobs)
-      results += process(jobs)
+      results += process(jobs, len(results))
 
-  sorter = lambda x: (-(x['num_Minerals'] + x['num_Vitamins']),
-                      x['price_per_serve'])
+  sorter = lambda x: x['price_per_g']
   results = sorted(results, key=sorter)
 
   print ('Saving {0} results'.format(len(results)))
@@ -302,26 +346,18 @@ def process_category(filename, category='multivitamins'):
   else:
     return results
 
-def process_one_multiV():
-  url = 'http://www.iherb.com/Deva-Multivitamin-Mineral-Supplement-Vegan-90-Coated-Tablets/12664'
-  url = 'http://www.iherb.com/Nature-s-Plus-Source-of-Life-Gold-Liquid-Delicious-Tropical-Fruit-Flavor-8-fl-oz-236-ml/22998'
-  url = 'http://www.iherb.com/Eclectic-Institute-Vita-Natal-Multi-Vitamin-Mineral-Formula-180-Tablets/15335'
-  url = 'http://www.iherb.com/Paradise-Herbs-ORAC-Energy-Earth-s-Blend-One-Daily-Superfood-Multivitamin-With-Iron-60-Veggie-Caps/42406'
-  url = 'http://www.iherb.com/Thorne-Research-Basic-Nutrients-2-Day-60-Veggie-Caps/52954'
+def process_one():
+  url = 'http://www.iherb.com/Nature-s-Alchemy-Tea-Tree-Essential-Oil-5-oz-15-ml/5752'
   r = requests.get(url)
   res = product_profile(r.text)
-  filename='test.json'
-  with open(filename, 'w') as outfile:
-    json.dump(res, outfile, indent=2)
+
   print(res)
 
 if __name__ == "__main__":
-  outfile = 'app/data/results.json'
+  category = 'tea-tree-oil'
+  outfile = 'app/data/{0}.json'.format(category)
   if len(sys.argv) > 1:
     outfile = sys.argv[1]
 
-  #postprocess(outfile)
-  #process_search_pages('digestives.json', 'enzymes', 7)
-  #process_search_pages(outfile, min_nutrients=1)
-  process_category(outfile)
-  #process_one_multiV()
+  #process_category(outfile, category=category)
+  process_one()
